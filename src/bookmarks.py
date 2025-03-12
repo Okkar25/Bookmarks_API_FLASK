606fc8aa7eb065f8132de4d4f1baba4da70ab1ec
Okkar25 <okkaaung2508@gmail.com>
2025-03-12 03:45:51 -0400

    bookmark post method - creating bookmarks

--- src/bookmarks.py
+++ src/bookmarks.py
@@ -1,3 +1,48 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from flask_jwt_extended import jwt_required, get_jwt_identity
+import validators 
+from src.database import db, Bookmark
+from src.constants.http_status_codes import (
+    HTTP_400_BAD_REQUEST, 
+    HTTP_409_CONFLICT,
+    HTTP_201_CREATED
+)
 
 bookmarks = Blueprint("bookmarks", __name__, url_prefix="/api/v1/bookmarks")
+
+@bookmarks.route("/", methods=["POST", "GET"])
+@jwt_required
+def handle_bookmarks():
+    current_user_id = get_jwt_identity()
+    
+    if request.method == "POST":
+        
+        body = request.get_json.get("body", "")
+        url = request.get_json.get("url", "")
+
+        if not validators.url(url):
+            return jsonify({
+                "error" : "Please enter a valid url !"
+            }), HTTP_400_BAD_REQUEST
+        
+        if Bookmark.query.filter_by(body=body).first():
+            return jsonify({
+                "error" : "This url already exists !"
+            }), HTTP_409_CONFLICT
+        
+        bookmark = Bookmark(body=body, url=url, user_id=current_user_id)
+        db.session.add(bookmark)
+        db.session.commit()
+
+        return jsonify({
+            "id" : bookmark.id,
+            "body" : bookmark.body,
+            "url" : bookmark.url,
+            "short_url" : bookmark.short_url,
+            "visit" : bookmark.visits,
+            "created_at" : bookmark.created_at,
+            "updated_at" : bookmark.updated_at
+        }), HTTP_201_CREATED
+    
+    else:
+        pass 
--- src/database.py
+++ src/database.py
@@ -24,6 +24,7 @@ class Bookmark(db.Model):
     short_url = db.Column(db.String(6), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id") )
     created_at = db.Column(db.DateTime, default = datetime.now())
+    updated_at = db.Column(db.DateTime, onupdate = datetime.now())
     visits = db.Column(db.DateTime, onupdate=datetime.now())
     
     def generate_short_characters(self):
