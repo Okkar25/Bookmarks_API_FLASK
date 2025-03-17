9f7a5dad9c2d5bcdadc1124b33f36bf9a00aa8c3
Okkar25 <okkaaung2508@gmail.com>
2025-03-16 21:44:21 -0400

    Swagger implementation in config and __init__ files

--- src/__init__.py
+++ src/__init__.py
@@ -8,6 +8,9 @@ from src.constants.http_status_codes import (
     HTTP_404_NOT_FOUND,
     HTTP_500_INTERNAL_SERVER_ERROR
 )
+from flasgger import Swagger, swag_from
+# swag_from enable us create yaml file to describe our spec 
+from src.config.swagger import swagger_config, template
 
 def create_app(test_config=None):
     
@@ -18,7 +21,12 @@ def create_app(test_config=None):
             SECRET_KEY=os.environ.get("SECRET_KEY"), 
             SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DB_URI"),
             SQLALCHEMY_TRACK_MODIFICATIONS=False,
-            JWT_SECRET_KEY=os.environ.get('JWT_SECRET_KEY')
+            JWT_SECRET_KEY=os.environ.get('JWT_SECRET_KEY'),
+            
+            SWAGGER = {
+                "title" : "Bookmarks API",
+                "uiversion" : 3
+            }
         )
     
     else:
@@ -36,6 +44,9 @@ def create_app(test_config=None):
     app.register_blueprint(auth)
     app.register_blueprint(bookmarks)
     
+    #Swagger
+    Swagger(app, config=swagger_config, template=template)
+    
     @app.get("/<short_url>")
     def redirect_to_url(short_url):
         bookmark = Bookmark.query.filter_by(short_url=short_url).first_or_404()
@@ -44,18 +55,13 @@ def create_app(test_config=None):
             bookmark.visits += 1 
             db.session.commit()
             return redirect(bookmark.url)
-            
-
+ 
+ 
     # Add a callback to check if a token is revoked
     @jwt.token_in_blocklist_loader
     def check_if_token_revoked(jwt_header, jwt_payload):
         jti = jwt_payload["jti"]
         return jti in revoked_tokens
-
-
-    
-    
-
 
     @app.errorhandler(HTTP_404_NOT_FOUND)
     def handle_404(e):
