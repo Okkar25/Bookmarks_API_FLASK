bbf88d2ae3a8c530be1366f25cd59c89588ba3c4
Okkar25 <okkaaung2508@gmail.com>
2025-03-22 23:09:38 -0400

    PUT and PATCH method seperated with different yaml files

--- src/bookmarks.py
+++ src/bookmarks.py
@@ -15,37 +15,61 @@ from flasgger import swag_from
 
 bookmarks = Blueprint("bookmarks", __name__, url_prefix="/api/v1/bookmarks")
 
-@bookmarks.route("/", methods=["POST", "GET"])
+@bookmarks.post("/")
 @jwt_required()
-def handle_bookmarks():
+@swag_from("./docs/bookmarks/create_bookmark.yaml")
+def create_bookmark():
     current_user_id = get_jwt_identity()
     
-    if request.method == "POST":
-        
-        body = request.get_json().get("body", "")
-        url = request.get_json().get("url", "")
+    body = request.get_json().get("body", "")
+    url = request.get_json().get("url", "")
 
-        if url and not validators.url(url):
-            return jsonify({
-                "error" : "Please enter a valid url !"
-            }), HTTP_400_BAD_REQUEST
-        
-        if url and Bookmark.query.filter_by(url=url).first():
-            return jsonify({
-                "error" : "This url already exists !"
-            }), HTTP_409_CONFLICT
-        
-        # new method - exits 
-        if db.session.query(exists().where(Bookmark.url == url)).scalar():
-             return jsonify({
-                "error" : "This url already exists !"
-            }), HTTP_409_CONFLICT
-        
-        bookmark = Bookmark(body=body, url=url, user_id=current_user_id)
-        db.session.add(bookmark)
-        db.session.commit()
-
+    if url and not validators.url(url):
         return jsonify({
+            "error" : "Please enter a valid url !"
+        }), HTTP_400_BAD_REQUEST
+    
+    if url and Bookmark.query.filter_by(url=url).first():
+        return jsonify({
+            "error" : "This url already exists !"
+        }), HTTP_409_CONFLICT
+    
+    # new method - exits 
+    if db.session.query(exists().where(Bookmark.url == url)).scalar():
+            return jsonify({
+            "error" : "This url already exists !"
+        }), HTTP_409_CONFLICT
+    
+    bookmark = Bookmark(body=body, url=url, user_id=current_user_id)
+    db.session.add(bookmark)
+    db.session.commit()
+
+    return jsonify({
+        "id" : bookmark.id,
+        "body" : bookmark.body,
+        "url" : bookmark.url,
+        "short_url" : bookmark.short_url,
+        "visit" : bookmark.visits,
+        "created_at" : bookmark.created_at,
+        "updated_at" : bookmark.updated_at
+    }), HTTP_201_CREATED
+
+
+@bookmarks.get("/")
+@jwt_required()
+@swag_from("./docs/bookmarks/get_bookmarks.yaml")
+def get_bookmarks():
+    current_user_id = get_jwt_identity()
+    
+    page = request.args.get("page", 1, type=int)
+    per_page = request.args.get("per_page", 5, type=int)
+    
+    bookmarks = Bookmark.query.filter_by(user_id=current_user_id).paginate(page=page, per_page=per_page)
+
+    data = []
+    
+    for bookmark in bookmarks.items:
+        data.append({
             "id" : bookmark.id,
             "body" : bookmark.body,
             "url" : bookmark.url,
@@ -53,45 +77,27 @@ def handle_bookmarks():
             "visit" : bookmark.visits,
             "created_at" : bookmark.created_at,
             "updated_at" : bookmark.updated_at
-        }), HTTP_201_CREATED
+        })
     
-    else:
-        page = request.args.get("page", 1, type=int)
-        per_page = request.args.get("per_page", 5, type=int)
-        
-        bookmarks = Bookmark.query.filter_by(user_id=current_user_id).paginate(page=page, per_page=per_page)
-
-        data = []
-        
-        for bookmark in bookmarks.items:
-            data.append({
-                "id" : bookmark.id,
-                "body" : bookmark.body,
-                "url" : bookmark.url,
-                "short_url" : bookmark.short_url,
-                "visit" : bookmark.visits,
-                "created_at" : bookmark.created_at,
-                "updated_at" : bookmark.updated_at
-            })
-        
-        meta = {
-            "page" : bookmarks.page,
-            "pages" : bookmarks.pages,
-            "total_count" : bookmarks.total,
-            "prev_page" : bookmarks.prev_num,
-            "next_page" : bookmarks.next_num,
-            "has_prev" : bookmarks.has_prev, 
-            "has_next" : bookmarks.has_next, 
-        }
-        
-        return jsonify({
-            "data" : data,
-            "meta" : meta
-        }), HTTP_200_OK
+    meta = {
+        "page" : bookmarks.page,
+        "pages" : bookmarks.pages,
+        "total_count" : bookmarks.total,
+        "prev_page" : bookmarks.prev_num,
+        "next_page" : bookmarks.next_num,
+        "has_prev" : bookmarks.has_prev, 
+        "has_next" : bookmarks.has_next, 
+    }
+    
+    return jsonify({
+        "data" : data,
+        "meta" : meta
+    }), HTTP_200_OK
 
 
 @bookmarks.get("/<int:id>")
 @jwt_required()
+@swag_from("./docs/bookmarks/get_bookmark.yaml")
 def get_bookmark(id):
     current_user_id = get_jwt_identity()
     
@@ -113,6 +119,7 @@ def get_bookmark(id):
 
 @bookmarks.delete("/<int:id>")
 @jwt_required()
+@swag_from("./docs/bookmarks/delete_bookmarks.yaml")
 def delete_bookmark(id):
     current_user_id = get_jwt_identity()
     
@@ -129,10 +136,16 @@ def delete_bookmark(id):
     }) , HTTP_204_NO_CONTENT
 
 
+
+
+
+
+
+
 @bookmarks.put("/<int:id>")
-@bookmarks.patch("/<int:id>")
 @jwt_required()
-def edit_bookmark(id):
+@swag_from("./docs/bookmarks/edit_bookmark_put.yaml")
+def put_bookmark(id):
     current_user_id = get_jwt_identity()
     
     bookmark_to_edit = Bookmark.query.filter_by(user_id=current_user_id, id=id).first()
@@ -166,6 +179,51 @@ def edit_bookmark(id):
         "updated_at" : bookmark_to_edit.updated_at
     }), HTTP_200_OK
 
+
+@bookmarks.patch("/<int:id>")
+@jwt_required()
+@swag_from("./docs/bookmarks/edit_bookmark_patch.yaml")
+def edit_bookmark(id):
+    current_user_id = get_jwt_identity()
+    
+    bookmark_to_edit = Bookmark.query.filter_by(user_id=current_user_id, id=id).first()
+
+    if not bookmark_to_edit:
+        return jsonify({"message" : "Item not found !"}), HTTP_404_NOT_FOUND
+    
+    # url = request.get_json().get("url", "")
+    # body = request.get_json().get("body", "")
+    
+    data = request.get_json()
+    
+    if not data: 
+        return jsonify({
+            "error" : "Please provide at least one field to update."
+        }), HTTP_400_BAD_REQUEST
+
+    if "url" in data:
+        if not validators.url(data["url"]):
+            return jsonify({
+                "error" : "Please enter a valid url !"
+            }), HTTP_400_BAD_REQUEST
+            
+        bookmark_to_edit.url = data["url"]
+
+    if "body" in data:    
+        bookmark_to_edit.body = data["body"]
+    
+    db.session.commit() 
+
+    return jsonify({
+        "id" : bookmark_to_edit.id,
+        "body" : bookmark_to_edit.body,
+        "url" : bookmark_to_edit.url,
+        "short_url" : bookmark_to_edit.short_url,
+        "visit" : bookmark_to_edit.visits,
+        "created_at" : bookmark_to_edit.created_at,
+        "updated_at" : bookmark_to_edit.updated_at
+    }), HTTP_200_OK
+    
 
 @bookmarks.get("/stats")
 @jwt_required()
